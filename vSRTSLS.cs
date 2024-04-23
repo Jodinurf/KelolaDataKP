9e0cb6a12e6a12fe9a4321277250bf4827e43e0b
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-04-23 16:56:47 +0700

    code untuk kelola data Surat Selesai KP

---
+++ vSRTSLS.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KelolaDataKP
+{
+    internal class vSRTSLS
+    {
+        public void Main()
+        {
+            vSRTSLS pr = new vSRTSLS();
+            Program prg = new Program();
+            while (true)
+            {
+                try
+                {
+                    SqlConnection conn = null;
+                    string strKoneksi = "Data source = DESKTOP-CKQPUM7\\JODINURF;" +
+                        "Initial catalog = KelolaDataKP;User ID = sa; Password = Jodinurf12";
+                    conn = new SqlConnection(strKoneksi);
+                    conn.Open();
+                    Console.Clear();
+                    while (true)
+                    {
+                        try
+                        {
+                            Console.WriteLine("\nMenu Kelola Data Surat Selesai SKP");
+                            Console.WriteLine("1. Melihat Seluruh Data");
+                            Console.WriteLine("2. Tambah Data");
+                            Console.WriteLine("3. Hapus Data");
+                            Console.WriteLine("4. Edit Data");
+                            Console.WriteLine("5. Cari Data");
+                            Console.WriteLine("6. Keluar");
+                            Console.WriteLine("\n Enter your choice (1-6): ");
+                            char ch = Convert.ToChar(Console.ReadLine());
+                            switch (ch)
+                            {
+                                case '1':
+                                    Console.Clear();
+                                    Console.WriteLine("Data Surat Selesai KP\n");
+                                    Console.WriteLine();
+                                    pr.baca(conn);
+                                    break;
+                                case '2':
+                                    Console.Clear();
+                                    Console.WriteLine("Input Data Surat Selesai KP\n");
+                                    Console.WriteLine("Masukkan KD Surat (15 characters): ");
+                                    string kdSurat = Console.ReadLine();
+                                    Console.WriteLine("Masukkan ID Dokumen (14 characters) : ");
+                                    string idDokumen = Console.ReadLine();
+                                    Console.WriteLine("Masukkan Disetujui Oleh : ");
+                                    string disetujuiOleh = Console.ReadLine();
+                                    Console.WriteLine("Masukkan Status (DITERIMA/DITOLAK) : ");
+                                    string status = Console.ReadLine().ToUpper();
+                                    Console.WriteLine("Masukkan Nama File DOC : ");
+                                    string fileDOC = Console.ReadLine();
+                                    try
+                                    {
+                                        pr.insert(kdSurat, idDokumen, disetujuiOleh, status, fileDOC, conn);
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nAnda tidak memiliki " +
+                                            "akses untuk menambah data");
+                                    }
+                                    break;
+                                case '3':
+                                    Console.Clear();
+                                    Console.WriteLine("Masukkan KD Surat yang ingin dihapus:\n");
+                                    string kdSuratHapus = Console.ReadLine();
+                                    try
+                                    {
+                                        pr.delete(kdSuratHapus, conn);
+                                    }
+                                    catch
+                                    {
+                                        Console.WriteLine("\nAnda tidak memiliki " +
+                                            "akses untuk menghapus data atau data yang anda masukkan salah");
+                                    }
+                                    break;
+                                case '4':
+                                    Console.Clear();
+                                    Console.WriteLine("Update Data Surat Selesai KP\n");
+                                    Console.WriteLine("Masukkan KD Surat yang akan diupdate: ");
+                                    string kdSuratToUpdate = Console.ReadLine();
+                                    Console.WriteLine("Masukkan Disetujui Oleh baru : ");
+                                    string newDisetujuiOleh = Console.ReadLine();
+                                    Console.WriteLine("Masukkan Status baru (DITERIMA/DITOLAK) : ");
+                                    string newStatus = Console.ReadLine().ToUpper();
+                                    Console.WriteLine("Masukkan Nama File DOC baru : ");
+                                    string newFileDOC = Console.ReadLine();
+                                    try
+                                    {
+                                        pr.update(kdSuratToUpdate, newDisetujuiOleh, newStatus, newFileDOC, conn);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("\nAnda tidak memiliki akses untuk mengubah data atau data yang anda masukkan salah");
+                                    }
+                                    break;
+                                case '5':
+                                    Console.Clear();
+                                    Console.WriteLine("Cari Data Surat Selesai KP Berdasarkan KD Surat\n");
+                                    Console.WriteLine("Masukkan KD Surat yang ingin dicari: ");
+                                    string searchKdSurat = Console.ReadLine();
+                                    try
+                                    {
+                                        pr.searchByKdSurat(searchKdSurat, conn);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("\nTerjadi kesalahan dalam pencarian data: " + e.Message);
+                                    }
+                                    break;
+                                case '6':
+                                    conn.Close();
+                                    Console.Clear();
+                                    //prg.Main(new string[0]);
+                                    return;
+                                default:
+                                    Console.Clear();
+                                    Console.WriteLine("\n Invalid option");
+                                    break;
+                            }
+                        }
+                        catch
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\nCheck for the value entered");
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tidak Dapat Mengakses Database Tersebut\n");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        public void baca(SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM SuratSelesaiKP", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                Console.WriteLine($"KD Surat: {r["kd_Surat"]}, ID Dokumen: {r["ID_Dokumen"]}, Disetujui Oleh: {r["DisetujuiOleh"]}, Kode Perusahaan: {r["kd_Perusahaan"]}, Status: {r["status"]}, File DOC: {r["fileDOC"]}");
+                Console.WriteLine();
+            }
+            r.Close();
+        }
+
+        public void insert(string kdSurat, string idDokumen, string disetujuiOleh, string status, string fileDOC, SqlConnection conn)
+        {
+            string str = "INSERT INTO SuratSelesaiKP (kd_Surat, ID_Dokumen, DisetujuiOleh, status, fileDOC) VALUES (@kdSurat, @idDokumen, @disetujuiOleh, @status, @fileDOC)";
+            SqlCommand cmd = new SqlCommand(str, conn);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("@kdSurat", kdSurat));
+            cmd.Parameters.Add(new SqlParameter("@idDokumen", idDokumen));
+            cmd.Parameters.Add(new SqlParameter("@disetujuiOleh", disetujuiOleh));
+            cmd.Parameters.Add(new SqlParameter("@status", status));
+            cmd.Parameters.Add(new SqlParameter("@fileDOC", fileDOC));
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil Ditambahkan");
+        }
+
+        public void delete(string kdSurat, SqlConnection con)
+        {
+            string str = "DELETE FROM SuratSelesaiKP WHERE kd_Surat = @kdSurat";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.Add(new SqlParameter("@kdSurat", kdSurat));
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Data Berhasil Dihapus");
+        }
+
+        public void update(string kdSurat, string newDisetujuiOleh, string newStatus, string newFileDOC, SqlConnection con)
+        {
+            string str = "UPDATE SuratSelesaiKP SET DisetujuiOleh = @newDisetujuiOleh, status = @newStatus, fileDOC = @newFileDOC WHERE kd_Surat = @kdSurat";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.CommandType = CommandType.Text;
+
+            cmd.Parameters.AddWithValue("@newDisetujuiOleh", newDisetujuiOleh);
+            cmd.Parameters.AddWithValue("@newStatus", newStatus);
+            cmd.Parameters.AddWithValue("@newFileDOC", newFileDOC);
+            cmd.Parameters.AddWithValue("@kdSurat", kdSurat);
+
+            int rowsAffected = cmd.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine("Data berhasil diupdate.");
+            }
+            else
+            {
+                Console.WriteLine("Data tidak ditemukan atau gagal diupdate.");
+            }
+        }
+
+        public void searchByKdSurat(string kdSurat, SqlConnection con)
+        {
+            string query = "SELECT * FROM SuratSelesaiKP WHERE kd_Surat = @kdSurat";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@kdSurat", kdSurat);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                Console.WriteLine("Hasil Pencarian:\n");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"KD Surat: {reader["kd_Surat"]}, ID Dokumen: {reader["ID_Dokumen"]}, Disetujui Oleh: {reader["DisetujuiOleh"]}, Kode Perusahaan: {reader["kd_Perusahaan"]}, Status: {reader["status"]}, File DOC: {reader["fileDOC"]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Data tidak ditemukan.");
+            }
+
+            reader.Close();
+        }
+    }
+}
